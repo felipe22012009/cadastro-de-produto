2856c3fc16679dd444a57bb3be686e502e33d43c
felipe22012009 <fernando.silva.ribeiro@escola.pr.gov.br>
2025-08-11 14:13:04 +0000

    aula1

---
+++ cadastro.py
@@ -0,0 +1,21 @@
+def formatar_nome(nome):
+    return nome.strip().title()
+
+def cadastrar_produto():
+    nome = input("Digite o nome do produto: ")
+    preco = float(input("Digite p preço do produto: "))
+    categoria = input("Digite a categoria do produto: ")
+    return (formatar_nome(nome), preco, categoria)
+
+def salvar_produto(produto):
+    with open("produtos.txt", "a", encoding="utf-8") as arquivo:
+        linha = f"{produto[0]};{produto[1]};{produto[2]}\n"
+        arquivo.write(linha)
+
+def listar_produtos():
+    try:
+        with open("produtos,txt", "r", encoding="utf-8") as arquivo:
+            for linha in arquivo:
+                nome, preco, categoria = linha.strip().strip(";")
+                print(f"Produto: {nome} | Preço: r${preco} | Categoria: {categoria}")
+    except
